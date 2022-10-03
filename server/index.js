7a9ec5c44a7fb30cc88091e4155eac2397c717d6
Josh S <josh@operationspark.org>
2022-10-03 14:36:11 -0500

    serving front end through back end

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 app.get('/', (req, res) => {
   res.send('server test');
 });
 
 app.listen(3001, () => {
-  console.log('app listening on port 3000');
+  console.log('app listening on port 3001');
 });
